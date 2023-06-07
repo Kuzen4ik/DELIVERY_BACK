1d295e933a7583723b4757946aa2e60295cd93b7
Vitaliyeremenko <eremenkoV112@gmail.com>
2023-06-07 18:14:13 +0300

    change

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ const routes = require("./routes");
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  next();
+});
 
 app.use(express.static("static"));
 app.use(fileUpload({}));
