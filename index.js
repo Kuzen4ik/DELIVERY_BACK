8176e02104f79b6f9f850ace7ff872ccf97c89f9
Vitaliyeremenko <eremenkoV112@gmail.com>
2023-06-08 13:19:17 +0300

    add compression

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const fileUpload = require("express-fileupload");
 const cors = require("cors");
 const routes = require("./routes");
+const compression = require("compression");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(compression());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -24,7 +26,7 @@ app.use(fileUpload({}));
 
 app.use("/api", routes);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
