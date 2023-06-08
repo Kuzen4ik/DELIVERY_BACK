0a4b73d62ce3d8e8f7d9bf4ed28ae82c73168d29
Vitaliyeremenko <eremenkoV112@gmail.com>
2023-06-08 10:27:53 +0300

    temp commit

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(fileUpload({}));
 
 app.use("/api", routes);
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
